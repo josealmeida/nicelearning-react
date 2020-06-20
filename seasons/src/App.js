e46ec61159fe3562bc1611e85b62446fa1e76c0c
Jose Hilario Almeida <josealmeida@gmail.com>
2020-06-20 13:54:51 +0100

    Change seasons app to use conditional rendering

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.error(error)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: '' };
+  }
 
-  return <div className="App">Hi</div>;
-};
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading</div>;
+  }
+}
 
 export default App;
