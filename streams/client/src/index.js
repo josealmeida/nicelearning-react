ce9aa913a111013bc26109ccc2f892605e2b757f
Jose Hilario Almeida <josealmeida@gmail.com>
2020-06-30 23:56:41 +0100

    Add redux form

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
@@ -9,7 +9,8 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
